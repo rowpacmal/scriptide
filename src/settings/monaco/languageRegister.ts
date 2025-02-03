a833a24de7dd3b2e023af4c2612b53124eb23590
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-03 18:31:17 +0100

    added extension prop

--- src/settings/monaco/languageRegister.ts
+++ src/settings/monaco/languageRegister.ts
@@ -3,7 +3,7 @@ import { KISS_VM_LANGUAGE } from '../../constants';
 
 // Language register config
 function languageRegister(monaco) {
-  monaco.languages.register({ id: KISS_VM_LANGUAGE });
+  monaco.languages.register({ id: KISS_VM_LANGUAGE, extensions: ['.kvm'] });
 }
 
 // Export
