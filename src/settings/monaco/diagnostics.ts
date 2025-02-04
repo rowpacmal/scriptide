cd45daf726d7da24d2b2e5f189cfce5b65fe51cb
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-04 17:24:57 +0100

    added diagnostics for variable names

--- src/settings/monaco/diagnostics.ts
+++ src/settings/monaco/diagnostics.ts
@@ -1,6 +1,9 @@
 // Import constants
 import { KISS_VM_LANGUAGE } from '../../constants';
 
+// Constants
+const LANGUAGE_CHECKER_ID = 'kvm';
+
 // Language register config
 function diagnostics(monaco) {
   function validateCustomLangModel(model) {
@@ -9,6 +12,7 @@ function diagnostics(monaco) {
     const code = model.getValue();
     const markers = [];
     const lines = code.split('\n');
+    // console.log(lines);
 
     lines.forEach((line, index) => {
       if (line.includes('ERROR')) {
@@ -19,23 +23,43 @@ function diagnostics(monaco) {
           endLineNumber: index + 1,
           endColumn: line.indexOf('ERROR') + 6,
           message: 'Detected ERROR in code.',
-          source: 'customLangChecker',
+          source: LANGUAGE_CHECKER_ID,
         });
       }
       if (line.includes('WARNING')) {
         markers.push({
           severity: monaco.MarkerSeverity.Warning,
-          startLineNumber: index + 2,
+          startLineNumber: index + 1,
           startColumn: line.indexOf('WARNING') + 1,
-          endLineNumber: index + 2,
+          endLineNumber: index + 1,
           endColumn: line.indexOf('WARNING') + 8,
           message: 'Detected WARNING.',
-          source: 'customLangChecker',
+          source: LANGUAGE_CHECKER_ID,
         });
       }
+
+      const code = line.replace(/\s+/g, ' ').trim().split(' ');
+      code.forEach((c, cindex) => {
+        if (c.includes('LET')) {
+          if (/([^a-z]+)/.test(code[cindex + 1])) {
+            // console.log(code[cindex + 1]);
+
+            markers.push({
+              severity: monaco.MarkerSeverity.ERROR,
+              startLineNumber: index + 1,
+              startColumn: line.indexOf(code[cindex + 1]) + 1,
+              endLineNumber: index + 1,
+              endColumn:
+                line.indexOf(code[cindex + 1]) + code[cindex + 1].length + 1,
+              message: 'Variable names must only consist of lowercase letters.',
+              source: LANGUAGE_CHECKER_ID,
+            });
+          }
+        }
+      });
     });
 
-    monaco.editor.setModelMarkers(model, 'customLangChecker', markers);
+    monaco.editor.setModelMarkers(model, LANGUAGE_CHECKER_ID, markers);
   }
 
   monaco.editor.onDidCreateModel((model) => {
