5e5cf3ce097d7929ea78af3449da0f4b5eceb25c
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-04 11:00:07 +0100

    added extra extension on language register

--- src/settings/monaco/languageRegister.ts
+++ src/settings/monaco/languageRegister.ts
@@ -3,7 +3,10 @@ import { KISS_VM_LANGUAGE } from '../../constants';
 
 // Language register config
 function languageRegister(monaco) {
-  monaco.languages.register({ id: KISS_VM_LANGUAGE, extensions: ['.kvm'] });
+  monaco.languages.register({
+    id: KISS_VM_LANGUAGE,
+    extensions: ['.kvm', '.extra.kvm'],
+  });
 }
 
 // Export
