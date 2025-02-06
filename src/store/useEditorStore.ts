ed2a86e17b752e51fbc49627c87dfa1ac3c0c15b
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-06 15:26:34 +0100

    updated editor store

--- src/store/useEditorStore.ts
+++ src/store/useEditorStore.ts
@@ -8,10 +8,15 @@ type TCode = string | null;
 interface IEditorStore {
   code: TCode;
   setCode: (code: TCode) => void;
+
   editorZoom: number;
   setEditorZoom: (editorZoom: number) => void;
+  editorZoomIn: () => void;
+  editorZoomOut: () => void;
+
   editorAutoSave: boolean;
   setEditorAutoSave: (editorAutoSave: boolean) => void;
+  toggleEditorAutoSave: () => void;
 }
 interface ILocalStorage {
   getItem: (key: string) => string;
@@ -22,12 +27,41 @@ interface ILocalStorage {
 const useEditorStore = create<IEditorStore>()((set) => ({
   code: null,
   setCode: (code: TCode) => set({ code }),
+
   editorZoom: Number(localStorage.getItem('editor-zoom')) || 0,
-  setEditorZoom: (editorZoom: number) => set({ editorZoom }),
+  setEditorZoom: (editorZoom: number) => {
+    set({ editorZoom });
+    localStorage.setItem('editor-zoom', editorZoom.toString());
+  },
+  editorZoomIn: () =>
+    set((state) => {
+      const zoomIn = state.editorZoom + 1;
+
+      localStorage.setItem('editor-zoom', zoomIn.toString());
+      return { editorZoom: zoomIn };
+    }),
+  editorZoomOut: () =>
+    set((state) => {
+      const zoomOut = state.editorZoom - 1;
+
+      localStorage.setItem('editor-zoom', zoomOut.toString());
+      return { editorZoom: zoomOut };
+    }),
+
   editorAutoSave:
     JSON.parse((localStorage as ILocalStorage).getItem('editor-auto-save')) ||
     false,
-  setEditorAutoSave: (editorAutoSave: boolean) => set({ editorAutoSave }),
+  setEditorAutoSave: (editorAutoSave: boolean) => {
+    set({ editorAutoSave });
+    localStorage.setItem('editor-auto-save', JSON.stringify(editorAutoSave));
+  },
+  toggleEditorAutoSave: () =>
+    set((state) => {
+      const saveState = !state.editorAutoSave;
+
+      localStorage.setItem('editor-auto-save', JSON.stringify(saveState));
+      return { editorAutoSave: saveState };
+    }),
 }));
 
 // Export
