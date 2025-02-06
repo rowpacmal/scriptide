c71268df68485e3b43e64484651f0e051dc99702
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-06 18:26:42 +0100

    created file and workspace store

---
+++ src/store/useFileStore.ts
@@ -0,0 +1,100 @@
+// Import dependencies
+import { create } from 'zustand';
+// Import libraries
+import minima from '@/lib/minima';
+// Import utilities
+import getFiles from '@/utils/getFiles';
+// Import stores
+import useEditorStore from './useEditorStore';
+import useWorkspaceStore from './useWorkspaceStore';
+
+// Types for the store
+type TCurrentFile = string | null;
+
+// Interface for the store
+interface IFileStore {
+  files: string[];
+  setFiles: (files: string[]) => void;
+
+  refreshFiles: (workspace: string) => Promise<void>;
+  addFile: (newFile: string) => Promise<void>;
+  saveFile: () => Promise<void>;
+  loadFile: (file: string) => Promise<void>;
+  deleteFile: (file: string) => Promise<void>;
+
+  currentFile: TCurrentFile;
+  setCurrentFile: (file: TCurrentFile) => void;
+}
+
+// Create the store
+const useFileStore = create<IFileStore>((set, get) => ({
+  files: [],
+  setFiles: (files) => set({ files }),
+
+  refreshFiles: async (workspace: string) => {
+    const files: string[] = await getFiles(`workspaces/${workspace}`);
+
+    set({ files });
+  },
+  addFile: async (newFile: string) => {
+    const currentWorkspace = useWorkspaceStore.getState().currentWorkspace;
+
+    if (!currentWorkspace) {
+      return;
+    }
+
+    await minima.file.save(`workspaces/${currentWorkspace}/${newFile}`, '');
+
+    get().refreshFiles(currentWorkspace);
+    set({ currentFile: newFile });
+    useEditorStore.setState({ code: '' });
+  },
+  saveFile: async () => {
+    const currentWorkspace = useWorkspaceStore.getState().currentWorkspace;
+    const currentFile = get().currentFile;
+
+    if (!currentWorkspace || !currentFile) {
+      return;
+    }
+
+    await minima.file.save(
+      `workspaces/${currentWorkspace}/${currentFile}`,
+      useEditorStore.getState().code
+    );
+  },
+  loadFile: async (file: string) => {
+    const currentWorkspace = useWorkspaceStore.getState().currentWorkspace;
+
+    if (!currentWorkspace) {
+      return;
+    }
+
+    const code = (
+      await minima.file.load(`workspaces/${currentWorkspace}/${file}`)
+    ).response.load.data;
+
+    useEditorStore.setState({ code });
+  },
+  deleteFile: async (file: string) => {
+    const currentWorkspace = useWorkspaceStore.getState().currentWorkspace;
+
+    if (!currentWorkspace) {
+      return;
+    }
+
+    await minima.file.delete(`workspaces/${currentWorkspace}/${file}`);
+
+    if (file === get().currentFile) {
+      set({ currentFile: null });
+      useEditorStore.setState({ code: null });
+    }
+
+    get().refreshFiles(currentWorkspace);
+  },
+
+  currentFile: null,
+  setCurrentFile: (file) => set({ currentFile: file }),
+}));
+
+// Export the store
+export default useFileStore;
---
+++ src/store/useWorkspaceStore.ts
@@ -0,0 +1,158 @@
+// Import dependencies
+import { create } from 'zustand';
+// Import libraries
+import minima from '@/lib/minima';
+// Import utilities
+import getFiles from '@/utils/getFiles';
+// Import stores
+import useEditorStore from './useEditorStore';
+import useFileStore from './useFileStore';
+
+// Types for the store
+type TCurrentWorkspace = string | null;
+
+// Interface for the store
+interface IWorkspaceStore {
+  workspaces: string[];
+  setWorkspace: (workspace: string[]) => void;
+
+  refreshWorkspaces: () => Promise<void>;
+  addWorkspace: (newWorkspace: string) => Promise<void>;
+  renameWorkspace: (newWorkspace: string) => Promise<void>;
+  copyWorkspace: (newWorkspace: string) => Promise<void>;
+  deleteWorkspace: () => Promise<void>;
+  deleteAllWorkspaces: () => Promise<void>;
+
+  currentWorkspace: TCurrentWorkspace;
+  setCurrentWorkspace: (workspace: TCurrentWorkspace) => void;
+}
+
+// Create the store
+const useWorkspaceStore = create<IWorkspaceStore>((set, get) => ({
+  workspaces: [],
+  setWorkspace: (workspaces: string[]) => set({ workspaces }),
+
+  refreshWorkspaces: async () => {
+    const workspaces: string[] = await getFiles('workspaces');
+    set({ workspaces });
+
+    const currentWorkspace = get().currentWorkspace;
+    const lastWorkspace = workspaces.at(-1);
+    set({ currentWorkspace: currentWorkspace || lastWorkspace || null });
+
+    if (!currentWorkspace || !lastWorkspace) {
+      useFileStore.setState({ files: [] });
+      return;
+    }
+
+    useFileStore.getState().refreshFiles(currentWorkspace || lastWorkspace);
+  },
+  addWorkspace: async (newWorkspace: string) => {
+    await minima.file.makedir(`workspaces/${newWorkspace}`);
+
+    set((state) => ({ workspaces: [...state.workspaces, newWorkspace] }));
+    set({ currentWorkspace: newWorkspace });
+
+    useFileStore.getState().refreshFiles(newWorkspace);
+    useFileStore.setState({ currentFile: null });
+    useEditorStore.setState({ code: null });
+  },
+  renameWorkspace: async (newWorkspace: string) => {
+    const currentWorkspace = get().currentWorkspace;
+
+    if (!currentWorkspace) {
+      return;
+    }
+
+    const currentFolder = `workspaces/${currentWorkspace}`;
+    const newFolder = `workspaces/${newWorkspace}`;
+    const files = useFileStore.getState().files;
+
+    if (files.length > 0) {
+      for (const file of files) {
+        await minima.file.move(
+          `${currentFolder}/${file}`,
+          `${newFolder}/${file}`
+        );
+      }
+    } else {
+      await minima.file.makedir(newFolder);
+    }
+
+    /** Quick fix to a renaming bug, where you can rename a workspace
+     * to the same name but in different case (lowercase or uppercase).
+     */
+    if (
+      newWorkspace.toLocaleLowerCase() !== currentWorkspace.toLocaleLowerCase()
+    ) {
+      await minima.file.delete(currentFolder);
+    }
+
+    set((state) => {
+      const workspaces = [...state.workspaces];
+      const indexOfCurrent = state.workspaces.indexOf(currentWorkspace);
+      workspaces[indexOfCurrent] = newWorkspace;
+
+      return { workspaces };
+    });
+    set({ currentWorkspace: newWorkspace });
+
+    useFileStore.getState().refreshFiles(newWorkspace);
+  },
+  copyWorkspace: async (newWorkspace: string) => {
+    const currentWorkspace = get().currentWorkspace;
+
+    if (!currentWorkspace) {
+      return;
+    }
+
+    const currentFolder = `workspaces/${currentWorkspace}`;
+    const newFolder = `workspaces/${newWorkspace}`;
+    const files = useFileStore.getState().files;
+
+    if (files.length > 0) {
+      for (const file of files) {
+        await minima.file.copy(
+          `${currentFolder}/${file}`,
+          `${newFolder}/${file}`
+        );
+      }
+    } else {
+      await minima.file.makedir(newFolder);
+    }
+
+    set((state) => ({ workspaces: [...state.workspaces, newWorkspace] }));
+    set({ currentWorkspace: newWorkspace });
+
+    useFileStore.getState().refreshFiles(newWorkspace);
+    useFileStore.setState({ currentFile: null });
+    useEditorStore.setState({ code: null });
+  },
+  deleteWorkspace: async () => {
+    await minima.file.delete(`workspaces/${get().currentWorkspace}`);
+    set({ currentWorkspace: null });
+
+    get().refreshWorkspaces();
+
+    useFileStore.setState({ currentFile: null });
+    useEditorStore.setState({ code: null });
+  },
+  deleteAllWorkspaces: async () => {
+    await minima.file.delete('workspaces');
+
+    set({ workspaces: [] });
+    set({ currentWorkspace: null });
+
+    useFileStore.setState({ files: [] }); // Quick fix to a bug
+    useFileStore.setState({ currentFile: null });
+    useEditorStore.setState({ code: null });
+  },
+
+  currentWorkspace: null,
+  setCurrentWorkspace: (currentWorkspace: TCurrentWorkspace) => {
+    set({ currentWorkspace });
+  },
+}));
+
+// Export the store
+export default useWorkspaceStore;
