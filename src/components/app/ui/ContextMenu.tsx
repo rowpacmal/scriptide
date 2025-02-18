68d5caccb812ea014e02486c1c76f10b8fa9484c
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-18 17:30:10 +0100

    moved file item context menu component to a separate file

---
+++ src/components/app/ui/ContextMenu.tsx
@@ -0,0 +1,96 @@
+import minima from '@/lib/minima';
+import useFileStore from '@/store/useFileStore';
+import useWorkspaceStore from '@/store/useWorkspaceStore';
+import { MenuDividerBase, MenuItemBase, MenuListBase } from './MenuListBase';
+import { LuDownload, LuPenLine, LuTrash } from 'react-icons/lu';
+
+// Constants (for debugging)
+const MINIDAPP_ID = (window as any).DEBUG_MINIDAPPID;
+
+function FileItemContextMenu({ file, setRenamingFile }) {
+  // Define store
+  const deleteFile = useFileStore((state) => state.deleteFile);
+
+  // Define handlers
+  /* Temporary fix to download files
+   *
+   * TODO - Improve download support in the future
+   */
+  async function handleDownload() {
+    // On Desktop
+    const fileName = file.name + '_minima_download_as_file_';
+    const url = `my_downloads/${fileName}`;
+    const debug = `https://${import.meta.env.VITE_DEBUG_HOST}:${
+      import.meta.env.VITE_DEBUG_MDS_PORT
+    }/${MINIDAPP_ID}/${url}`;
+
+    await minima.file.copytoweb(file.location, `/${url}`);
+
+    const link = document.createElement('a');
+    link.href = import.meta.env.VITE_DEBUG === 'true' ? debug : url;
+    document.body.appendChild(link);
+    link.click();
+  }
+
+  return (
+    <MenuListBase>
+      <MenuItemBase
+        // label="Rename File"
+        icon={<LuPenLine />}
+        onClick={() => setRenamingFile(true)}
+      >
+        Rename
+      </MenuItemBase>
+
+      <MenuItemBase
+        // label="Delete File"
+        icon={<LuTrash />}
+        onClick={() => deleteFile(file)}
+      >
+        Delete
+      </MenuItemBase>
+
+      {/* TODO - Add copy and download support */}
+      {/* <MenuDividerBase />
+
+      <MenuItemBase
+        // label="Copy File"
+        icon={<LuCopy />}
+        onClick={() => {}}
+        disabled
+      >
+        Copy
+      </MenuItemBase>
+
+      <MenuItemBase
+        // label="Copy File Name"
+        icon={<LuCopy />}
+        onClick={() => {}}
+        disabled
+      >
+        Copy name
+      </MenuItemBase>
+
+      <MenuItemBase
+        // label="Copy File Path"
+        icon={<LuCopy />}
+        onClick={() => {}}
+        disabled
+      >
+        Copy path
+      </MenuItemBase> */}
+
+      <MenuDividerBase />
+
+      <MenuItemBase
+        // label="Download File"
+        icon={<LuDownload />}
+        onClick={handleDownload}
+      >
+        Download
+      </MenuItemBase>
+    </MenuListBase>
+  );
+}
+
+export { FileItemContextMenu };
--- src/components/app/ui/FileItem.tsx
+++ src/components/app/ui/FileItem.tsx
@@ -11,123 +11,13 @@ import { useEffect, useState } from 'react';
 import {
   // LuBug,
   // LuCopy,
-  LuDownload,
   LuFile,
-  LuPenLine,
-  LuTrash,
 } from 'react-icons/lu';
-// Import store
-import useFileStore from '@/store/useFileStore';
 // Import components
-import { MenuDividerBase, MenuItemBase, MenuListBase } from './MenuListBase';
-import minima from '@/lib/minima';
-import useWorkspaceStore from '@/store/useWorkspaceStore';
 import FileItemRename from './FileItemRename';
-
-// Constants (for debugging)
-const MINIDAPP_ID = (window as any).DEBUG_MINIDAPPID;
+import { FileItemContextMenu } from './ContextMenu';
 
 // File item context menu component
-function FileItemContextMenu({ file, setRenamingFile }) {
-  // Define store
-  const currentWorkspace = useWorkspaceStore((state) => state.currentWorkspace);
-  const deleteFile = useFileStore((state) => state.deleteFile);
-
-  // Define handlers
-  /* Temporary fix to download files
-   *
-   * TODO - Improve download support in the future
-   */
-  async function handleDownload() {
-    // On Desktop
-    const fileName = file + '_minima_download_as_file_';
-    const url = `my_downloads/${fileName}`;
-    const debug = `https://${import.meta.env.VITE_DEBUG_HOST}:${
-      import.meta.env.VITE_DEBUG_MDS_PORT
-    }/${MINIDAPP_ID}/${url}`;
-
-    await minima.file.copytoweb(
-      `workspaces/${currentWorkspace}/${file}`,
-      `/${url}`
-    );
-
-    const link = document.createElement('a');
-    link.href = import.meta.env.VITE_DEBUG === 'true' ? debug : url;
-    document.body.appendChild(link);
-    link.click();
-  }
-
-  return (
-    <MenuListBase>
-      <MenuItemBase
-        // label="Rename File"
-        icon={<LuPenLine />}
-        onClick={() => setRenamingFile(true)}
-      >
-        Rename
-      </MenuItemBase>
-
-      <MenuItemBase
-        // label="Delete File"
-        icon={<LuTrash />}
-        onClick={() => deleteFile(file)}
-      >
-        Delete
-      </MenuItemBase>
-
-      {/* TODO - Add copy and download support */}
-      {/* <MenuDividerBase />
-
-      <MenuItemBase
-        // label="Copy File"
-        icon={<LuCopy />}
-        onClick={() => {}}
-        disabled
-      >
-        Copy
-      </MenuItemBase>
-
-      <MenuItemBase
-        // label="Copy File Name"
-        icon={<LuCopy />}
-        onClick={() => {}}
-        disabled
-      >
-        Copy name
-      </MenuItemBase>
-
-      <MenuItemBase
-        // label="Copy File Path"
-        icon={<LuCopy />}
-        onClick={() => {}}
-        disabled
-      >
-        Copy path
-      </MenuItemBase> */}
-
-      <MenuDividerBase />
-
-      <MenuItemBase
-        // label="Download File"
-        icon={<LuDownload />}
-        onClick={handleDownload}
-      >
-        Download
-      </MenuItemBase>
-
-      {/* Used for debugging during development  */}
-      {/* <MenuDividerBase />
-
-      <MenuItemBase
-        // label="Debug"
-        icon={<LuBug />}
-        onClick={() => console.log(file)}
-      >
-        Debug
-      </MenuItemBase> */}
-    </MenuListBase>
-  );
-}
 
 // File item component
 function FileItem({ children, file, onClick, isActive = false }) {
