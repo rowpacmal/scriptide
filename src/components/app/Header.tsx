c96e5354bcc7ca1fda94873476981dca190ab9b7
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-24 10:58:03 +0100

    added grid to the header

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -1,6 +1,6 @@
 // Dependencies
 import useAppTheme from '@/themes/useAppTheme';
-import { Button, HStack, Text, useColorMode } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Text, useColorMode } from '@chakra-ui/react';
 import { LuMoon, LuSun } from 'react-icons/lu';
 
 // App header component
@@ -10,24 +10,38 @@ function Header() {
 
   // Render
   return (
-    <HStack
+    <Grid
       as="header"
       h="2rem"
       borderBottom="1px solid"
       borderColor={borderColor}
-      justify="space-between"
       px={2}
+      templateColumns="repeat(3, 1fr)"
     >
-      <div></div>
+      <GridItem
+        colStart={2}
+        colEnd={3}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text as="h1" fontSize="lg">
+          Minima Script IDE
+        </Text>
+      </GridItem>
 
-      <Text as="h1" fontSize="lg">
-        Minima Script IDE
-      </Text>
-
-      <Button size="xs" variant="ghost" onClick={toggleColorMode}>
-        {colorMode === 'light' ? <LuSun /> : <LuMoon />}
-      </Button>
-    </HStack>
+      <GridItem
+        colStart={3}
+        colEnd={4}
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-end"
+      >
+        <Button size="xs" variant="ghost" onClick={toggleColorMode}>
+          {colorMode === 'light' ? <LuSun /> : <LuMoon />}
+        </Button>
+      </GridItem>
+    </Grid>
   );
 }
 
