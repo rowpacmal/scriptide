a9a23fed1aad4397960059c761e100fa7ab87fa5
rowpacmal-minima <rowel.malmstrom@minima.global>
2025-02-28 12:14:38 +0100

    updated settings gui

--- src/components/ui/panels/SettingsPanel.tsx
+++ src/components/ui/panels/SettingsPanel.tsx
@@ -8,44 +8,48 @@ import {
   useColorMode,
   VStack,
 } from '@chakra-ui/react';
+import { AccordionBase, AccordionItemBase } from '../systems/AccordionBase';
+import { LuPalette } from 'react-icons/lu';
 
 function SettingsPanel() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { accent, bgAlt, borderColor, color, colorAlt } = useAppTheme();
+  const { accent, borderColor, color, colorAlt } = useAppTheme();
 
   return (
     <VStack w="100%" h="100%" fontSize="sm" gap={3}>
-      <VStack w="100%" gap={1}>
-        <Text
-          as="h3"
-          w="100%"
-          textTransform="uppercase"
-          fontSize="xs"
-          color={colorAlt}
+      <AccordionBase>
+        <AccordionItemBase
+          title="Appearance"
+          icon={<LuPalette />}
+          isTop
+          isBottom
         >
-          Appearance
-        </Text>
-
-        <HStack w="100%" gap={2} justify="space-between" flexWrap="wrap">
-          <Text as="h4" color={colorAlt}>
-            Color mode
-          </Text>
-
-          <Box>
-            <Select
-              size="xs"
-              value={colorMode}
-              onChange={toggleColorMode}
-              borderColor={borderColor}
-              _hover={{ borderColor: color }}
-              _focusVisible={{ borderColor: accent }}
+          <HStack w="100%" gap={2} justify="space-between" flexWrap="wrap">
+            <Text
+              as="h4"
+              textTransform="uppercase"
+              fontSize="xs"
+              color={colorAlt}
             >
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-            </Select>
-          </Box>
-        </HStack>
-      </VStack>
+              Color mode
+            </Text>
+
+            <Box>
+              <Select
+                size="xs"
+                value={colorMode}
+                onChange={toggleColorMode}
+                borderColor={borderColor}
+                _hover={{ borderColor: color }}
+                _focusVisible={{ borderColor: accent }}
+              >
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+              </Select>
+            </Box>
+          </HStack>
+        </AccordionItemBase>
+      </AccordionBase>
     </VStack>
   );
 }
